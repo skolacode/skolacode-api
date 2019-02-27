5c4163388732b0d7aafce030f15ffd71b7905bb6
ibrahim <aibrahim3546@gmail.com>
2019-02-28 00:22:09 +0800

    add jwt for testing

--- routes/auth.js
+++ routes/auth.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const passport = require('passport');
+const jwt = require('jsonwebtoken');
 
 const router = express.Router();
 
@@ -13,6 +14,24 @@ router.get('/github/callback',
 
 router.get('/github/failed', (req, res) => {
 	res.send('FAILED');
+});
+
+router.get('/github/token/sign', (req, res) => {
+	const token = jwt.sign({
+		email: 'test',
+	}, 'secret');
+	res.send(token);
+});
+
+router.get('/github/:token', (req, res) => {
+	const { token } = req.params;
+	jwt.verify(token, 'secret', function(err, decoded) {
+		if (err) {
+			console.log('ERR => ', err);
+		}
+		console.log('DECODE => ', decoded.email);
+	});
+	res.send('VERI');
 });
 
 module.exports = router;
