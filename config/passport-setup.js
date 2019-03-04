06de2045ba944a72353ce5172d918dd4e7ac0604
ibrahim <aibrahim3546@gmail.com>
2019-03-04 23:36:38 +0800

    add change status endpoint

--- config/passport-setup.js
+++ config/passport-setup.js
@@ -4,6 +4,12 @@ const GitHubStrategy = require('passport-github2').Strategy;
 const User = require('../models/user');
 const { github } = require('./keys');
 
+const checkValue = (val) => {
+	if (val) {
+		return val;
+	}
+	return '';
+};
 
 passport.use(
 	new GitHubStrategy({
@@ -18,19 +24,20 @@ passport.use(
 			if (err) {
 				return done(err, null);
 			}
+			const { id, username, displayName } = profile;
+			const { avatar_url, bio } = profile._json;
 
 			if (!user) {
-				User.create({
-					githubID: profile.id,
-					github: profile.username,
-				}, (e, u) => {
-					if (!e) {
-						return done(e, u);
-					}
-				});
-			} else {
-				return done(err, user);
-			}	
+				return User.create({
+					githubID: id,
+					github: checkValue(username),
+					displayName: checkValue(displayName),
+					bio: checkValue(bio),
+					avatarUrl: checkValue(avatar_url),
+				}, (e, u) => done(e, u));
+			}
+			
+			return done(err, user);
 		});
 	})
 );
--- routes/article.js
+++ routes/article.js
@@ -2,7 +2,7 @@ const express = require('express');
 const { check, validationResult } = require('express-validator/check');
 
 const router = express.Router();
-const { userAuthentication, userVerification } = require('../middlewares');
+const { userAuthentication, userVerification, userRoleVerification } = require('../middlewares');
 const Article = require('../models/article');
 const { INTERNAL_SERVER_ERROR } = require('../constants/errors');
 
@@ -64,9 +64,11 @@ router.post('/', [
 router.patch('/:id', [
 	userAuthentication,
 	userVerification,
-	check('title').not().isEmpty(),
-	check('headerImgUrl').not().isEmpty(),
-	check('content').not().isEmpty(),
+	check('title').isString(),
+	check('headerImgUrl').isString(),
+	check('content').isString(),
+	check('tags').isArray(),
+	check('isPublished').isBoolean(),
 ], (req, res) => {
 	const errors = validationResult(req);
 	if (!errors.isEmpty()) {
@@ -100,6 +102,30 @@ router.delete('/:id', [userAuthentication, userVerification], (req, res) => {
 			return res.status(500).json({ error: { message: INTERNAL_SERVER_ERROR } });
 		}
 		res.json({ message: 'SUCCESS' });
+	});
+});
+
+router.patch('/status/:id', [
+	userAuthentication,
+	userRoleVerification,
+	check('status').isString(),
+], (req, res) => {
+	const errors = validationResult(req);
+	if (!errors.isEmpty()) {
+		return res.status(422).json({ errors: errors.array() });
+	}
+
+	const { status } = req.body;
+	const { id } = req.params;
+
+	Article.findByIdAndUpdate(id, {
+		status,
+	}, (err, article) => {
+		if (err) {
+			return res.status(500).json({ error: { message: INTERNAL_SERVER_ERROR } });
+		}
+
+		res.redirect(`/api/v1/article/${article._id}`);
 	});
 });
 
